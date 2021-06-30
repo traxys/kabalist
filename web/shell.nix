f0999394fcc6678ca3881b31c04e6b67ec3c6961
Quentin Boyer <quentin+dev@familleboyer.net>
2021-06-30 21:59:53 +0200

    web: fix nix.shell

--- web/shell.nix
+++ web/shell.nix
@@ -3,11 +3,12 @@ let
   vue_cli = import "/home/traxys/softs/vue-cli/packages/@vue/cli/default.nix" { };
 in
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs.nodePackages; [
+  nativeBuildInputs = with pkgs.nodePackages; with pkgs; [
     vue_cli.package
     vls
     prettier
     eslint
-	npm
+	  npm
+    nodejs
   ];
 }
