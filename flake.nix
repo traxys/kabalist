acddd8cf6f48191ddc025f3962b0302acc0fc068
traxys <quentin+dev@familleboyer.net>
2022-06-12 22:09:11 +0200

    Add a global lockfile

--- flake.nix
+++ flake.nix
@@ -3,12 +3,14 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.android-nixpkgs.url = "github:tadfisher/android-nixpkgs";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
     android-nixpkgs,
+    rust-overlay,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
@@ -16,12 +18,17 @@
         config = {
           android_sdk.accept_license = true;
         };
+        overlays = [(import rust-overlay)];
       };
     in {
       devShell = with pkgs;
         mkShell {
           nativeBuildInputs = [pkgs.bashInteractive];
+
+		  DATABASE_URL="postgres://traxys:traxys@localhost/list";
+
           buildInputs = [
+		  	# Flutter
             (android-nixpkgs.sdk."${system}" (sdkPkgs:
               with sdkPkgs; [
                 cmdline-tools-latest
@@ -35,6 +42,15 @@
             flutter
             jdk8
             dart
+
+			# Rust
+            (rust-bin.stable.latest.default.override {
+              targets = ["wasm32-unknown-unknown"];
+            })
+
+			# Web
+			trunk
+			wasm-bindgen-cli
           ];
         };
     });
